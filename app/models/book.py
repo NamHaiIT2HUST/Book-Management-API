edc6377de1d1dfa2dfceaf10b3f4f8cc817bc989
Nem Hai <namhai23092005@gmail.com>
2026-02-27 11:22:31 +0700

    Create Model SQLAlchemy

---
+++ app/models/book.py
@@ -0,0 +1,25 @@
+from sqlalchemy import Column, Integer, String, Text, ForeignKey, DateTime
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
+
+from app.db.base import Base
+
+class Book(Base):
+    __tablename__ = "books"
+
+    id = Column(Integer, primary_key = True, index = True)
+    name = Column(String(255), nullable = False, index = True)
+    description = Column(Text, nullable = False)
+    published_year = Column(Integer, nullable = False)
+    
+    author_id = Column(Integer, ForeignKey("authors.id", ondelete = "RESTRICT"), nullable = False)
+    category_id = Column(Integer, ForeignKey("categories.id", ondelete = "RESTRICT"), nullable = False)
+
+    cover_image = Column(String(255), nullable = True) #save path, example: "static/covers/xxx.jpg"
+
+    created_at = Column(DateTime(timezone = True), server_default = func.now(), nullable = False)
+    updated_at = Column(DateTime(timezone = True), server_default = func.now(), onupdate = func.now(), nullable = False)
+
+    #Relationship Author, Category
+    author = relationship("Author", back_populates = "books")
+    category = relationship("Category",back_populates = "books")
---
+++ app/models/category.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String, Text
+from sqlalchemy.orm import relationship
+
+from app.db.base import Base
+
+class Category(Base):
+    __tablename__ = "categories"
+
+    id = Column(Integer, primary_key = True, index = True)
+    title = Column(String(250), nullable = False)
+    description = Column(Text, nullable = False)
+
+    #Relationship 1-n with Book
+    books = relationship("Book", back_populates = "category")
